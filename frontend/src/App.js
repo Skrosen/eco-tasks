0b1e396a39b0a01f0422df72099c1ac65e3e4aa8
Skrosen <76884712+Skrosen@users.noreply.github.com>
2022-01-19 18:28:42 +0100

    feat: add routes, add content to all components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return <h1>heloooo</h1>;
-}
+import Main from "./components/Main";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Leaderboard from "./components/Leaderboard";
+import InfoPage from "./components/InfoPage";
+import UserProfile from "./components/UserProfile";
+import Feedback from "./components/Feedback";
+import NotFound from "./components/NotFound";
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/eco-information" element={<InfoPage />} />
+				<Route path="/leaderboard" element={<Leaderboard />} />
+				<Route path="/:user" element={<UserProfile />} />
+				<Route path="/feedback" element={<Feedback />} />
+				<Route path="/*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
