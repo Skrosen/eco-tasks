7121230c0c02514614e25e1e54963930e7b004cf
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-01-20 13:51:49 +0100

    create function to delete user

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ const UserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    lowercase: true,
     unique: true,
     required: true,
   },
@@ -208,6 +209,33 @@ app.post("/login", async (req, res) => {
         userId: user._id,
         username: user.username,
         accessToken: user.accessToken,
+      });
+    } else {
+      res.status(404).json({
+        message: "User not found or password incorrect",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      message: "Something went wrong...",
+      success: false,
+    });
+  }
+});
+
+app.get("/deleteuser", authenticateUser);
+app.get("/deleteuser", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      await User.deleteOne({ username });
+      res.status(200).json({
+        message: `${username} is deleted`,
+        success: true,
       });
     } else {
       res.status(404).json({
