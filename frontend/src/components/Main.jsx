b214c0290d5e55ba42e10b84e4407a37bf549b37
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-01-20 14:48:00 +0100

    feat: add reusable button

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Button from "./reusable-components/Button";
+
 const Main = () => {
-	return <h1>Main</h1>;
+  return (
+    <>
+      <h1>Main</h1>
+      <Button text="text" />
+    </>
+  );
 };
 
 export default Main;
