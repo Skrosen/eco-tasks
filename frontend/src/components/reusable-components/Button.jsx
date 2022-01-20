7c71aa211e4daa819b144db3d88508cddf9f418b
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-01-20 14:39:24 +0100

    chore: change .js to .jsx

---
+++ frontend/src/components/reusable-components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const Button = styled.button``;
+
+const Feedback = () => {
+  return <h1>Feedback</h1>;
+};
+
+export default Feedback;
