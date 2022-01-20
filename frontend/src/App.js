53df76257e6e152b2014e29583162b23b80664f2
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-01-20 11:36:25 +0100

    feat: add title to page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,27 +4,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
-import Leaderboard from "./components/Leaderboard";
+import Leaderboard from "./components/LeaderBoard";
 import InfoPage from "./components/InfoPage";
 import UserProfile from "./components/UserProfile";
 import Feedback from "./components/Feedback";
 import NotFound from "./components/NotFound";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Main />} />
-				<Route path="/signup" element={<Signup />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/eco-information" element={<InfoPage />} />
-				<Route path="/leaderboard" element={<Leaderboard />} />
-				<Route path="/:user" element={<UserProfile />} />
-				<Route path="/feedback" element={<Feedback />} />
-				<Route path="/*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/eco-information" element={<InfoPage />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/:user" element={<UserProfile />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
