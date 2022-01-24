01b92931d9f053a278af73751201186e708f2eca
Skrosen <76884712+Skrosen@users.noreply.github.com>
2022-01-24 16:07:43 +0100

    feat: add tasks reducer and page

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchTasks } from "../reducers/tasks";
 
 const Tasks = () => {
-	return <h1>Tasks</h1>;
+	const dispatch = useDispatch();
+	const tasks = useSelector((store) => store?.tasks?.tasks?.response);
+	useEffect(() => {
+		dispatch(fetchTasks());
+	}, []);
+	return (
+		<>
+			<h1>Tasks hello</h1>
+			{tasks?.length &&
+				tasks.map((task) => <p key={task.title}>{task.title}</p>)}
+		</>
+	);
 };
 
 export default Tasks;
---
+++ frontend/src/reducers/tasks.js
@@ -0,0 +1,79 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { ui } from "./ui";
+import { API_URL } from "../utils/urls";
+
+const tasks = createSlice({
+	name: "tasks",
+	initialState: {
+		tasks: [],
+		error: null,
+	},
+	reducers: {
+		setTasks: (store, action) => {
+			store.tasks = action.payload;
+		},
+		// setCurrentStep: (store, action) => {
+		// 	store.currentStep = action.payload;
+		// },
+		// setSteps: (store, action) => {
+		// 	let latestStep = action.payload;
+		// 	latestStep = { ...latestStep, id: id };
+		// 	store.steps.push(latestStep);
+		// },
+		// setStartPosition: (store, action) => {
+		// 	store.startingPosition = action.payload;
+		// },
+		// setHistory: (store, action) => {
+		// 	const latestStep = {
+		// 		...store.currentStep,
+		// 		directionTaken: action.payload,
+		// 	};
+		// 	store.steps.push(latestStep);
+		// },
+		// setInitialState: () => {
+		// 	return initialState;
+		// },
+	},
+});
+
+export const fetchTasks = () => {
+	return (dispatch) => {
+		dispatch(ui.actions.setLoading(true));
+		fetch(API_URL("tasks"))
+			.then((res) => res.json())
+			.then((json) => {
+				dispatch(tasks.actions.setTasks(json));
+				dispatch(ui.actions.setLoading(false));
+			});
+	};
+};
+
+// export const fetchSteps = (direction) => {
+// 	return (dispatch, getState) => {
+// 		dispatch(ui.actions.setLoading(true));
+// 		const state = getState();
+// 		dispatch(steps.actions.setHistory(direction));
+
+// 		const infoToAPI = {
+// 			username: state.steps.username,
+// 			type: "move",
+// 			direction: direction,
+// 		};
+// 		const options = {
+// 			method: "POST",
+// 			headers: {
+// 				"Content-Type": "application/json",
+// 			},
+// 			body: JSON.stringify(infoToAPI),
+// 		};
+
+// 		fetch("https://wk16-backend.herokuapp.com/action", options)
+// 			.then((res) => res.json())
+// 			.then((json) => {
+// 				dispatch(steps.actions.setCurrentStep(json));
+// 				dispatch(ui.actions.setLoading(false));
+// 			});
+// 	};
+// };
+
+export default tasks;
