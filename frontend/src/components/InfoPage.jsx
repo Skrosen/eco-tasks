2f40cc47b03fd2baa7303ddf33c7aea0244bbaca
Skrosen <76884712+Skrosen@users.noreply.github.com>
2022-01-29 13:55:12 +0100

    fix: rendering of info in InfoPage

--- frontend/src/components/InfoPage.jsx
+++ frontend/src/components/InfoPage.jsx
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { API_URL } from "../utils/urls";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const InfoPage = () => {
 	const [info, setInfo] = useState({});
-	const dispatch = useDispatch();
 	const accessToken = useSelector((store) => store.user.accessToken);
 
 	useEffect(() => {
-		dispatch(GetEcoInfo(accessToken));
+		GetEcoInfo();
 	}, []);
 
 	const GetEcoInfo = () => {
 		const options = {
 			headers: { Authorization: accessToken },
 		};
+
 		fetch(API_URL("information"), options)
 			.then((res) => res.json())
 			.then((data) => {
-				setInfo(data);
+				setInfo(data.response);
 			});
 	};
 	console.log(info);
@@ -27,6 +27,16 @@ const InfoPage = () => {
 	return (
 		<>
 			<h1>Eco information</h1>
+			{info?.length &&
+				info.map((info) => (
+					<div key={info._id}>
+						<h3>
+							{info.title} - ({info.category})
+						</h3>
+						<p>{info.description}</p>
+						<p>Source: {info.source}</p>
+					</div>
+				))}
 		</>
 	);
 };
