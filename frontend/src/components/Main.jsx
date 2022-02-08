4f0ac118633a57108a18edd8a1709a8191ffbf37
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-02-08 16:57:14 +0100

    fix: remove unused imports

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,37 +1,40 @@
 import React from "react";
 
-import { Button } from "./reusable-components/Buttons";
 import { MainContainer } from "./reusable-components/Containers";
 
 const Main = () => {
-	return (
-		<MainContainer>
-			<h1>Eco Friendly Page</h1>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et tristique
-			odio. Donec metus ex, ullamcorper ut turpis non, commodo venenatis arcu.
-			Sed blandit, leo sed aliquet commodo, ligula metus efficitur lorem, vitae
-			suscipit justo justo in neque. Sed a leo ornare, tristique odio quis,
-			aliquam neque. Proin cursus, quam eget maximus mattis, elit purus pharetra
-			augue, in consequat urna tortor in arcu. Nunc ullamcorper risus non quam
-			imperdiet laoreet. Nunc aliquam neque eget eros varius posuere.
-			Suspendisse elementum tellus at ante interdum, ut lobortis urna efficitur.
-			Suspendisse ultrices, lectus sit amet viverra ullamcorper, tortor erat
-			luctus tortor, nec iaculis justo mi eu massa. Ut dapibus ex ac arcu
-			finibus, vel dictum lacus congue. Phasellus suscipit imperdiet nisi
-			sagittis aliquet. Quisque urna mauris, dictum vel metus eu, mollis
-			malesuada lectus. Etiam eget pellentesque ante, sit amet pharetra leo.
-			Integer egestas ante tortor, eu sollicitudin libero iaculis id. Etiam nec
-			laoreet massa. Suspendisse venenatis lobortis bibendum. Quisque dui augue,
-			ultricies ut mauris et, finibus efficitur tortor. Ut vitae nibh sed nulla
-			tristique porta. Aenean ullamcorper vitae erat quis suscipit. Fusce massa
-			eros, fringilla nec eros eget, efficitur gravida nibh. Aenean sit amet
-			tellus nisi. Duis vehicula pulvinar est non varius. In tincidunt, purus ac
-			tincidunt posuere, leo felis convallis leo, eleifend gravida turpis urna
-			nec ipsum. Orci varius natoque penatibus et magnis dis parturient montes,
-			nascetur ridiculus mus. Nam ac aliquet diam. Integer volutpat enim non
-			tincidunt pulvinar.
-		</MainContainer>
-	);
+  return (
+    <MainContainer>
+      <h1>Eco Friendly Page</h1>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et
+      tristique odio. Donec metus ex, ullamcorper ut turpis non,
+      commodo venenatis arcu. Sed blandit, leo sed aliquet commodo,
+      ligula metus efficitur lorem, vitae suscipit justo justo in
+      neque. Sed a leo ornare, tristique odio quis, aliquam neque.
+      Proin cursus, quam eget maximus mattis, elit purus pharetra
+      augue, in consequat urna tortor in arcu. Nunc ullamcorper risus
+      non quam imperdiet laoreet. Nunc aliquam neque eget eros varius
+      posuere. Suspendisse elementum tellus at ante interdum, ut
+      lobortis urna efficitur. Suspendisse ultrices, lectus sit amet
+      viverra ullamcorper, tortor erat luctus tortor, nec iaculis
+      justo mi eu massa. Ut dapibus ex ac arcu finibus, vel dictum
+      lacus congue. Phasellus suscipit imperdiet nisi sagittis
+      aliquet. Quisque urna mauris, dictum vel metus eu, mollis
+      malesuada lectus. Etiam eget pellentesque ante, sit amet
+      pharetra leo. Integer egestas ante tortor, eu sollicitudin
+      libero iaculis id. Etiam nec laoreet massa. Suspendisse
+      venenatis lobortis bibendum. Quisque dui augue, ultricies ut
+      mauris et, finibus efficitur tortor. Ut vitae nibh sed nulla
+      tristique porta. Aenean ullamcorper vitae erat quis suscipit.
+      Fusce massa eros, fringilla nec eros eget, efficitur gravida
+      nibh. Aenean sit amet tellus nisi. Duis vehicula pulvinar est
+      non varius. In tincidunt, purus ac tincidunt posuere, leo felis
+      convallis leo, eleifend gravida turpis urna nec ipsum. Orci
+      varius natoque penatibus et magnis dis parturient montes,
+      nascetur ridiculus mus. Nam ac aliquet diam. Integer volutpat
+      enim non tincidunt pulvinar.
+    </MainContainer>
+  );
 };
 
 export default Main;
