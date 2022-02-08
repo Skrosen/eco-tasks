52ab33709e58b7404690520d73a19d2165830acd
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-02-08 14:29:44 +0100

    styling: update styling of button

--- frontend/src/components/reusable-components/Buttons.jsx
+++ frontend/src/components/reusable-components/Buttons.jsx
@@ -2,50 +2,49 @@ import React from "react";
 import styled from "styled-components/macro";
 
 export const Btn = styled.button`
-	position: relative;
-	display: inline-block;
-	margin: 10px;
-	padding: 10px 10px;
-	text-align: center;
-	font-family: inherit;
-	font-size: 20px;
-	letter-spacing: 1px;
-	text-decoration: none;
-	color: ${(props) => props.theme.greyGreen};
-	background: ${(props) => props.theme.mediumGreen};
-	transition: ease-out 0.5s;
-	border: 2px solid;
-	border-color: ${(props) => props.theme.darkGreen};
-	border-radius: 10px;
-	box-shadow: inset 0 0 0 0;
-	cursor: pointer;
+  position: relative;
+  display: inline-block;
+  margin: 10px;
+  padding: 5px 10px;
+  text-align: center;
+  font-family: inherit;
+  font-size: 18px;
+  text-decoration: none;
+  color: ${(props) => props.theme.greyGreen};
+  background: ${(props) => props.theme.mediumGreen};
+  border: none;
+  border-radius: 6px;
+  box-shadow: inset 0 0 0 0;
+  cursor: pointer;
 
-	&:hover {
-		color: ${(props) => props.theme.hoverBeige};
-		box-shadow: inset 0 -100px 0 0 ${(props) => props.theme.mediumGreen};
-	}
+  &:hover {
+    color: ${(props) => props.theme.hoverBeige};
+    box-shadow: inset 0 -100px 0 0 ${(props) => props.theme.darkGreen};
+  }
 
-	&:active {
-		transform: scale(0.9);
-	}
+  &:active {
+    transform: scale(0.9);
+  }
 `;
 
 export const TimeSpanButton = styled.button`
-	all: unset;
-	margin-left: 10px;
-	color: ${(props) => (props.selected ? props.theme.mediumGreen : "black")};
-	text-decoration: ${(props) => (props.selected ? "underline" : "none")};
+  all: unset;
+  margin-left: 10px;
+  color: ${(props) =>
+    props.selected ? props.theme.mediumGreen : "black"};
+  text-decoration: ${(props) =>
+    props.selected ? "underline" : "none"};
 
-	&:hover {
-		cursor: pointer;
-		color: ${(props) => props.theme.mediumGreen};
-	}
+  &:hover {
+    cursor: pointer;
+    color: ${(props) => props.theme.mediumGreen};
+  }
 
-	&:focus {
-		color: ${(props) => props.theme.mediumGreen};
-	}
+  &:focus {
+    color: ${(props) => props.theme.mediumGreen};
+  }
 `;
 
 export const Button = ({ text, onClick }) => {
-	return <Btn onClick={onClick}>{text}</Btn>;
+  return <Btn onClick={onClick}>{text}</Btn>;
 };
