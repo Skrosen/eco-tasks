a07892b3b9446506ae99994a1178aa18c1bc1c2d
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-02-10 13:55:49 +0100

    styling/feat: style container and add description of page to main

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -8,7 +8,18 @@ const Main = () => {
       <h1>Welcome to EcoTasks</h1>
       <p>
         Do you struggle to find what you can do for the environment in
-        your day-to-day life?
+        your day-to-day life? &#9989;
+      </p>
+      <p>
+        Do you want to keep track of what you have done for the
+        environment, while getting points for every task you perform?
+        &#9989;
+      </p>
+      <p>
+        Then welcome to EcoTasks! We have gathered several different
+        tasks which have an positive impact on the environment. You
+        can also find information on why they are good tasks to do,
+        and see other users scores at the leaderboard.
       </p>
     </MainContainer>
   );
